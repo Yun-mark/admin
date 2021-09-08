5348e98ca1ee064cd57fbe624c6ff33a0b9cff00
mark <1304101539@qq.com>
2021-09-08 16:19:35 +0800

    用户登录-创建页面组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// @表示src目录，不受当前文件路径影响
+import Login from '@/views/login/index.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/login', component: Login
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/login/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login-container">123   </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginIndex'
+}
+</script>
+
+<style>
+
+</style>
